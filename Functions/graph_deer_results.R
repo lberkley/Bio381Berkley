9003d142586e66f211c1f1ed841668c4fb7a553b
Lauren <lauren.berkley@uvm.edu>
2022-03-23 15:01:59 -0400

    add to homework 9

--- Functions/graph_deer_results.R
+++ Functions/graph_deer_results.R
@@ -7,14 +7,11 @@
 library(tidyverse)
 library(ggplot2)
 
-graph_deer_results <- function(x_var=runif(10),
-                          y_var=runif(10)) {
-  d_frame <- data.frame(x_var,y_var)
+graph_deer_results <- function(data=Deer_Data) {
   
-  reg_plot <- ggplot(data=d_frame) +
-    aes(x=x_var,y=y_var) +
-    geom_point()
-  stat_smooth(method=lm,se=0.95)
-  print(reg_plot)
+  bar_plot <- ggplot(data=data) +
+    aes(x=State,y=InfectionRate) +
+    geom_boxplot()
+  print(bar_plot)
   message("regression data plotted")
 }
